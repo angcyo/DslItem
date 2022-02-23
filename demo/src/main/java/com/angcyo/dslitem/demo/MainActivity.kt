43c78e42a07c9a92c896c2d6e3424fb3f65b6be8
angcyo <angcyo@126.com>
2022-02-23 14:40:26 +0800

    * fix demo

--- demo/src/main/java/com/angcyo/dslitem/demo/MainActivity.kt
+++ demo/src/main/java/com/angcyo/dslitem/demo/MainActivity.kt
@@ -13,79 +13,80 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         findViewById<RecyclerView>(R.id.recycler_view)?.initDslAdapter {
-            DslBaseEditItem()() {
-                itemEditText = this::class.java.simpleName
-                _lastEditSelectionStart = itemEditText?.length ?: -1
+            render {
+                DslBaseEditItem()() {
+                    itemEditText = this::class.java.simpleName
+                    _lastEditSelectionStart = itemEditText?.length ?: -1
 
-                configEditTextStyle {
-                    hint = "请输入..."
+                    configEditTextStyle {
+                        hint = "请输入..."
+                    }
+                }
+
+                DslBaseInfoItem()() {
+                    itemInfoText = this::class.java.simpleName
+                }
+
+                DslBaseLabelItem()() {
+                    itemLabelText = this::class.java.simpleName
+                }
+
+                DslButtonItem()() {
+                    itemButtonText = this::class.java.simpleName
+                }
+
+                DslButtonItem()() {
+                    itemButtonText = this::class.java.simpleName
+                    configButtonStyle {
+                        themeStyle()
+                    }
+                }
+                DslButtonItem()() {
+                    itemButtonText = this::class.java.simpleName
+                    configButtonStyle {
+                        fillStyle()
+                    }
+                }
+                DslButtonItem()() {
+                    itemButtonText = this::class.java.simpleName
+                    configButtonStyle {
+                        solidStyle()
+                    }
+                }
+
+                DslGridItem()() {
+                    itemText = this::class.java.simpleName
+                    itemIcon = R.drawable.png
+                }
+
+                DslLabelEditItem()() {
+                    itemLabelText = "Label"
+                    itemEditText = this::class.java.simpleName
+                    _lastEditSelectionStart = itemEditText?.length ?: -1
+
+                    configEditTextStyle {
+                        hint = "请输入..."
+                    }
+                }
+
+                DslLabelTextItem()() {
+                    itemLabelText = "Label"
+                    itemText = this::class.java.simpleName
+                }
+
+                DslSwitchInfoItem()() {
+                    itemInfoText = this::class.java.simpleName
+                }
+
+                DslTextInfoItem()() {
+                    itemInfoText = this::class.java.simpleName
+                    itemDarkText = "DarkText"
+                }
+
+                DslTextItem()() {
+                    itemText = this::class.java.simpleName
                 }
             }
-
-            DslBaseInfoItem()() {
-                itemInfoText = this::class.java.simpleName
-            }
-
-            DslBaseLabelItem()() {
-                itemLabelText = this::class.java.simpleName
-            }
-
-            DslButtonItem()() {
-                itemButtonText = this::class.java.simpleName
-            }
-
-            DslButtonItem()() {
-                itemButtonText = this::class.java.simpleName
-                configButtonStyle {
-                    themeStyle()
-                }
-            }
-            DslButtonItem()() {
-                itemButtonText = this::class.java.simpleName
-                configButtonStyle {
-                    fillStyle()
-                }
-            }
-            DslButtonItem()() {
-                itemButtonText = this::class.java.simpleName
-                configButtonStyle {
-                    solidStyle()
-                }
-            }
-
-            DslGridItem()() {
-                itemText = this::class.java.simpleName
-                itemIcon = R.drawable.png
-            }
-
-            DslLabelEditItem()() {
-                itemLabelText = "Label"
-                itemEditText = this::class.java.simpleName
-                _lastEditSelectionStart = itemEditText?.length ?: -1
-
-                configEditTextStyle {
-                    hint = "请输入..."
-                }
-            }
-
-            DslLabelTextItem()() {
-                itemLabelText = "Label"
-                itemText = this::class.java.simpleName
-            }
-
-            DslSwitchInfoItem()() {
-                itemInfoText = this::class.java.simpleName
-            }
-
-            DslTextInfoItem()() {
-                itemInfoText = this::class.java.simpleName
-                itemDarkText = "DarkText"
-            }
-
-            DslTextItem()() {
-                itemText = this::class.java.simpleName
-            }
-
         }
     }
 }
